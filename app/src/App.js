cf7ab63c7c4cf00c8fc2dd6392be18af1be501fc
asliddin200524006 <149239813+asliddin200524006@users.noreply.github.com>
2024-03-22 17:03:36 +0600

    Add files via upload

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import Modal from './Modal';
+
+function App() {
+  return (
+    <div className="App">
+      <Modal
+      title={'weather'}
+      opis={'beatifull very good'}
+      price={100}
+      img={'https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?cs=srgb&dl=pexels-lukas-rodriguez-3680219.jpg&fm=jpg'}
+      />
+    </div>
+  );
+}
+
+export default App;
